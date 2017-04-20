fa1030d4c4e3d301a9318452631ee9a34c6a8eeb
suharaki <kuroi_@hotmail.co.jp>
2017-04-20 11:51:46 +0700

    Test Map

--- app/src/main/java/sdu/kamonthip/connectmapp/CallMapp.java
+++ app/src/main/java/sdu/kamonthip/connectmapp/CallMapp.java
@@ -1,7 +1,11 @@
 package sdu.kamonthip.connectmapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CallMapp extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class CallMapp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_mapp);
-    }
+    } //onCreat Method
+    public void  runCallMap(View view){
+        //Intitail View
+
+        EditText latitude = (EditText) findViewById(R.id.mapLat);
+        EditText longtitude = (EditText) findViewById(R.id.mapLong);
+        EditText label = (EditText) findViewById(R.id.mapLabel);
+
+
+        String lat = latitude.getText().toString().trim();
+        String lng = longtitude.getText().toString().trim();
+        String mlabel = label.getText().toString().trim();
+
+
+        Uri location = Uri.parse("http://maps.google.com/maps?q=loc:13.7597237,100.5392481");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,location);
+        //MapIntent.setPackage("com.google.android.apps.maps");//ใช้ไม่ได้กับ gynemotion
+        startActivity(mapIntent);
+
+    }//runCalMap
 }
